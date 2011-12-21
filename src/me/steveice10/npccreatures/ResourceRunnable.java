dddadf9f10a0b9b28811ed400c53097997df1745
Steveice10 <Steveice10@gmail.com>
2011-12-21 09:27:53 -0800

    Removing old folders.

--- src/me/steveice10/npccreatures/ResourceRunnable.java
+++
@@ -1,9 +0,0 @@
-package me.steveice10.npccreatures;
-
-public abstract class ResourceRunnable implements Runnable {
-	protected Object[] params;
-	
-	public ResourceRunnable(Object... parameters) {
-		params = parameters;
-	}
-}
